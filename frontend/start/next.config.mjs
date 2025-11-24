a1cd56ace49f899a9cc15c8b63d5bfc79dad3b2a
amin abdi <amin.s2254@gmail.com>
2025-11-24 19:40:06 +0330

    initial project

--- frontend/start/next.config.mjs
+++ frontend/start/next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "5000",
-        pathname: "/uploads/**",
-      },
-    ],
-  },
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
