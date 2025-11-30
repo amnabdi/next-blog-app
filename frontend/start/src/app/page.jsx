1dba936cab3aa4d0e75d0bbd2bd942d07ae152a4
amin abdi <amin.s2254@gmail.com>
2025-11-30 14:06:39 +0330

    set tailwind configv

---
+++ frontend/start/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-amber-300 text-2xl font-mono">Hello World</h1>
+      <div className="bg-primary-300">sd</div>
+    </div>
+  );
+}
